dc975d513267927a271ed04c260f691b873bcaf2
Nimarta123 <140687219+Nimarta123@users.noreply.github.com>
2025-05-04 20:56:48 +0500

    Add files via upload

---
+++ task01.py
@@ -0,0 +1,11 @@
+def generate_fibonacci(n): #function is defined here.
+    fib_series = [] #list is given 
+    a, b = 0, 1 #variables.
+    for _ in range(n): #loop 
+        fib_series.append(a) #object identified.
+        a, b = b, a + b
+    return fib_series
+
+n_terms = 10
+print("Fibonacci Series:", generate_fibonacci(n_terms)) #printing in the last.
+ #now let's see the results.
